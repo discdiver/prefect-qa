267f9f653ff7517d632f99882ac5ad206f8c99b7
Jeff Hale <jeffmshale@gmail.com>
2022-08-30 18:29:05 -0400

    test code for qa prefect 2.3

---
+++ demo-gh.py
@@ -0,0 +1,28 @@
+import pandas
+import yfinance as yf
+from datetime import timedelta
+from prefect import flow, task
+
+
+@task(retries=3, cache_expiration=timedelta(30))
+def fetch_data(ticker):
+    return yf.download(ticker)
+
+
+def save_data(stock_df):
+    stock_df.to_csv("output.csv")
+
+
+@flow
+def pipeline4(ticker="AMZN"):
+
+    # from prefect.blocks.notifications import SlackWebhook
+
+    # slack_webhook_block = SlackWebhook.load("msg-jeff")
+    # slack_webhook_block.notify("Hello from Prefect!")
+    df = fetch_data(ticker)
+    save_data(df)
+
+
+if __name__ == "__main__":
+    pipeline4()
