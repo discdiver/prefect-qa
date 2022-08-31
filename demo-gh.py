1223ec4cb6bf0ef4262071cefd76bcb05b37f722
Jeff Hale <jeffmshale@gmail.com>
2022-08-31 15:02:30 -0400

    add logger

--- demo-gh.py
+++ demo-gh.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 from datetime import timedelta
 from prefect import flow, task
+from prefect.logging import get_run_logger
 
 
 @task(retries=3, cache_expiration=timedelta(30))
@@ -11,16 +12,14 @@ def fetch_data(ticker):
 
 def save_data(stock_df):
     stock_df = pd.read_csv("data.csv")
+    log = get_run_logger()
+    log(stock_df)
+    print(stock_df)
     stock_df.to_csv("output.csv")
 
 
 @flow
 def pipeline4(ticker="AMZN"):
-
-    # from prefect.blocks.notifications import SlackWebhook
-
-    # slack_webhook_block = SlackWebhook.load("msg-jeff")
-    # slack_webhook_block.notify("Hello from Prefect!")
     df = fetch_data(ticker)
     save_data(df)
 
